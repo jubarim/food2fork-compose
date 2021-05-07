8bda337a5ceabc6e5545b1c30c77d1b7ffc6868d
Joao Marinho Assis <jubarim@gmail.com>
2021-05-07 20:15:58 -0300

    2.2 Compose-only Navigation System

--- app/src/main/java/com/codingwithmitch/food2forkcompose/presentation/navigation/Screen.kt
+++ app/src/main/java/com/codingwithmitch/food2forkcompose/presentation/navigation/Screen.kt
@@ -1,9 +1,9 @@
 package com.codingwithmitch.food2forkcompose.presentation.navigation
 
 sealed class Screen(
-    route: String
-) {
-    object RecipeList : Screen("recipeList")
+    val route: String,
+){
+    object RecipeList: Screen("recipeList")
 
-    object RecipeDetail : Screen("recipeDetail")
+    object RecipeDetail: Screen("recipeDetail")
 }
---
+++ app/src/main/java/com/codingwithmitch/food2forkcompose/presentation/ui/recipe/RecipeDetailScreen.kt
@@ -0,0 +1,61 @@
+package com.codingwithmitch.food2forkcompose.presentation.ui.recipe
+
+import android.util.Log
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.codingwithmitch.food2forkcompose.util.TAG
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@Composable
+fun RecipeDetailScreen(
+    isDarkTheme: Boolean,
+    recipeId: Int?,
+    viewModel: RecipeViewModel
+) {
+    Log.d(TAG, "RecipeDetailScreen: $viewModel")
+
+    Text("RecipeDetailScreen: $recipeId ")
+
+//    val loading = viewModel.loading.value
+//
+//    val recipe = viewModel.recipe.value
+//
+//    val scaffoldState = rememberScaffoldState()
+//
+//    AppTheme(
+//        displayProgressBar = loading,
+//        scaffoldState = scaffoldState,
+//        darkTheme = application.isDark.value,
+//    ){
+//        Scaffold(
+//            scaffoldState = scaffoldState,
+//            snackbarHost = {
+//                scaffoldState.snackbarHostState
+//            }
+//        ) {
+//            Box (
+//                modifier = Modifier.fillMaxSize()
+//            ){
+//                if (loading && recipe == null) LoadingRecipeShimmer(imageHeight = IMAGE_HEIGHT.dp)
+//                else recipe?.let {
+//                    if(it.id == 1) { // force an error to demo snackbar
+//                        snackbarController.getScope().launch {
+//                            snackbarController.showSnackbar(
+//                                scaffoldState = scaffoldState,
+//                                message = "An error occurred with this recipe",
+//                                actionLabel = "Ok"
+//                            )
+//                        }
+//                    }
+//                    else{
+//                        RecipeView(
+//                            recipe = it,
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//    }
+}
